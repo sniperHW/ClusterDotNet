218ed77c8769455ef505f1066417fd5e7367ca79
sniperHW <sniperHW@163.com>
2022-11-07 18:10:56 +0800

    rpc

--- Message.cs
+++ Message.cs
@@ -31,7 +31,7 @@ public class ProtoMessage
             }
         }
 
-        public IMessage Unmarshal(int id,byte[] buff)
+        public IMessage Unmarshal(int id,byte[] buff,int offset,int length)
         {
             if(!idToMeta.ContainsKey(id)){
                 throw new Exception($"id:{id} not register");
@@ -39,7 +39,7 @@ public class ProtoMessage
 
             Func<IMessage> factory = idToMeta[id];
             IMessage o = factory();
-            MemoryStream memoryStream = new MemoryStream(buff, 0, buff.Length);
+            MemoryStream memoryStream = new MemoryStream(buff, offset, length);
             o.MergeFrom(memoryStream); 
             return o;
         }
@@ -78,12 +78,12 @@ public class ProtoMessage
         meta.Register<T>(id,message);    
     }
 
-    public static IMessage Unmarshal(string ns,int id,byte[] buff)
+    public static IMessage Unmarshal(string ns,int id,byte[] buff,int offset,int length)
     {
         if(!Namespace.ContainsKey(ns)){
             throw new Exception($"namespace:{ns} not exists");
         }
-        return Namespace[ns].Unmarshal(id,buff);
+        return Namespace[ns].Unmarshal(id,buff,offset,length);
     }
 
     public static  void Marshal(string ns,IMessage message,MemoryStream stream) 
@@ -105,11 +105,14 @@ public class ProtoMessage
 
 public class MessageConstont 
 {
-    public static  readonly int SizeLen       = 4; 
-	public static  readonly int SizeFlag      = 1;
-	public static  readonly int SizeToAndFrom = 8;
-	public static  readonly int SizeCmd       = 2;
-	public static  readonly int SizeRpcSeqNo  = 8;
+    public static  readonly int SizeLen       = sizeof(int); 
+	public static  readonly int SizeFlag      = sizeof(byte);
+
+    public static readonly  int SizeLogicAddr = sizeof(int);
+
+	public static  readonly int SizeToAndFrom = SizeLogicAddr*2;
+	public static  readonly int SizeCmd       = sizeof(ushort);
+	public static  readonly int SizeRpcSeqNo  = sizeof(ulong);
 	public static  readonly int MinSize       = SizeLen + SizeFlag + SizeToAndFrom;
 
 
@@ -121,10 +124,10 @@ public class MessageConstont
 
 }
 
-public class Message : MessageI
+public class SSMessage : MessageI
 {
-	private UInt16 _cmd = 0;
-    public UInt16 Cmd{get=>_cmd;}
+	private ushort _cmd = 0;
+    public ushort Cmd{get=>_cmd;}
 
     private LogicAddr _to;
     public LogicAddr To{get=>_to;}
@@ -132,32 +135,56 @@ public class Message : MessageI
     private LogicAddr _from;
     public LogicAddr From{get=>_from;}
 
-    private Object? _payload;
-    public Object? Payload{get=>_payload;}
+    private IMessage _payload;
+    public IMessage Payload{get=>_payload;}
 
     public void Encode(MemoryStream stream)
     {
+        var position1 = stream.Position;
+        try{
+            stream.Write(BitConverter.GetBytes(0));//写入占位符
+            byte flag = (byte)(0 | MessageConstont.Msg);
+            stream.WriteByte(flag);
+            stream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((int)_to.ToUint32())));
+            stream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((int)_from.ToUint32())));
+            stream.Write(BitConverter.GetBytes((UInt16)IPAddress.HostToNetworkOrder((int)_cmd)));
+            var position2 = stream.Position;
+            ProtoMessage.Marshal("ss",_payload,stream);
+            var pbSize = stream.Position - position2;
+            var position3 = stream.Position;
+            stream.Position = position1;
+            var sizePayload = MessageConstont.SizeFlag + MessageConstont.SizeToAndFrom + MessageConstont.SizeCmd;
+            if(sizePayload+MessageConstont.SizeLen > MessageConstont.MaxPacketSize) {
+                throw new Exception("packet too large");
+            }   
+            stream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder(sizePayload)));
+            stream.Position = position3;
+        } catch (Exception e) {
+            Console.WriteLine(e);
+            stream.Position = position1;
+        }
     }
 
-    public Message(LogicAddr to,LogicAddr from,Object payload)
+
+    public SSMessage(LogicAddr to,LogicAddr from,IMessage payload)
     {
         _to = to;
         _from = from;
         _payload = payload;
+        _cmd = (ushort)ProtoMessage.GetID("ss",payload);
     }
 
-
-    public Message(LogicAddr to,LogicAddr from,UInt16 cmd, Object payload)
+    public SSMessage(LogicAddr to,LogicAddr from,ushort cmd,IMessage payload)
     {
+        _to = to;
+        _from = from;
+        _payload = payload;
         _cmd = cmd;
-        _to = to;
-        _from = from;
-        _payload = payload;
     }
 
 } 
 
-public class RelayMessage
+public class RelayMessage : MessageI
 {
     private LogicAddr _to;
     public LogicAddr To{get => _to;}
@@ -168,14 +195,15 @@ public class RelayMessage
     private byte[] _payload;
     public byte[] Payload{get => _payload;}
 
-    public RpcRequestMessage? GetRpcRequest(){
+    public Rpc.Proto.rpcRequest? GetRpcRequest(){
         if(((int)_payload[0] & MessageConstont.MaskMessageType) != MessageConstont.RpcReq){
             try
             {
-                MemoryStream memoryStream = new MemoryStream(_payload, 5, _payload.Length - 5);
+                var offset = MessageConstont.SizeLen + MessageConstont.SizeFlag;
+                MemoryStream memoryStream = new MemoryStream(_payload, offset, _payload.Length - offset);
                 Rpc.Proto.rpcRequest req = new Rpc.Proto.rpcRequest();
                 req.MergeFrom(memoryStream); 
-                return new RpcRequestMessage(req.Seq,req.Method,req.Arg.ToByteArray());
+                return req;
             }
             catch (Exception)
             {
@@ -195,9 +223,225 @@ public class RelayMessage
 
     public void Encode(MemoryStream stream)
     {
-        var lenBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(_payload.Length));
-        stream.Write(lenBytes,0,lenBytes.Length);
-        stream.Write(_payload,0,_payload.Length);
+        var position1 = stream.Position;
+        try{
+            var lenBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(_payload.Length));
+            stream.Write(lenBytes,0,lenBytes.Length);
+            stream.Write(_payload,0,_payload.Length);
+        } catch (Exception e) {
+            Console.WriteLine(e);
+            stream.Position = position1;
+        }
+    }
+}
+
+
+public class RpcRequestMessage : MessageI
+{
+
+    private LogicAddr _to;
+    public LogicAddr To{get=>_to;}
+
+    private LogicAddr _from;
+    public LogicAddr From{get=>_from;}
+
+    private Rpc.Proto.rpcRequest _req;  
+
+    public RpcRequestMessage(LogicAddr to,LogicAddr from,Rpc.Proto.rpcRequest req)
+    {
+        _to = to;
+        _from = from;
+        _req = req;
+    }  
+
+    public void Encode(MemoryStream stream)
+    {
+        var position1 = stream.Position;
+        try{
+            MemoryStream tmpstream = new MemoryStream();
+            _req.WriteTo(tmpstream);
+            var payloadLen = MessageConstont.SizeFlag + MessageConstont.SizeToAndFrom + (int)tmpstream.Length;
+			var totalLen = MessageConstont.SizeLen + payloadLen;
+            if(payloadLen+MessageConstont.SizeLen > MessageConstont.MaxPacketSize) {
+                throw new Exception("packet too large");
+            }
+            stream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder(payloadLen)));
+            byte flag = (byte)(0 | MessageConstont.RpcReq);
+            stream.WriteByte(flag);
+            stream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((int)_to.ToUint32())));
+            stream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((int)_from.ToUint32()))); 
+            stream.Write(tmpstream.ToArray());   
+        }  catch (Exception e) {
+            Console.WriteLine(e);
+            stream.Position = position1;
+        }
+    }
+}
+
+public class RpcResponseMessage : MessageI
+{
+
+    private LogicAddr _to;
+    public LogicAddr To{get=>_to;}
+
+    private LogicAddr _from;
+    public LogicAddr From{get=>_from;}
+
+    private Rpc.Proto.rpcResponse _resp;
+
+    public Rpc.Proto.rpcResponse Resp{get=>_resp;}
+
+    public RpcResponseMessage(LogicAddr to,LogicAddr from,Rpc.Proto.rpcResponse resp)
+    {
+        _to = to;
+        _from = from;        
+        _resp = resp;
+    }
+
+    public void Encode(MemoryStream stream)
+    {
+        var position1 = stream.Position;
+        try{
+            MemoryStream tmpstream = new MemoryStream();
+            _resp.WriteTo(tmpstream);
+            var payloadLen = MessageConstont.SizeFlag + MessageConstont.SizeToAndFrom + (int)tmpstream.Length;
+			var totalLen = MessageConstont.SizeLen + payloadLen;
+            if(payloadLen+MessageConstont.SizeLen > MessageConstont.MaxPacketSize) {
+                throw new Exception("packet too large");
+            }
+            stream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder(payloadLen)));
+            byte flag = (byte)(0 | MessageConstont.RpcResp);
+            stream.WriteByte(flag);
+            stream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((int)_to.ToUint32())));
+            stream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((int)_from.ToUint32()))); 
+            stream.Write(tmpstream.ToArray());   
+        }  catch (Exception e) {
+            Console.WriteLine(e);
+            stream.Position = position1;
+        }
+    }
+}
+
+
+public interface MessageCodecI 
+{
+    public Object? Decode(byte[] buff,int offset,int length);
+}
+
+public class SSMessageCodec
+{
+
+    private uint localLogicAddr;
+
+    public SSMessageCodec(uint localLogicAddr) 
+    {
+        this.localLogicAddr = localLogicAddr;
+    }
+
+    public Object? Decode(byte[] buff,int offset,int length)
+    {
+        var backOffset = offset;
+        var endOffset = offset + length;
+        if(length<MessageConstont.SizeFlag + MessageConstont.SizeToAndFrom) {
+            throw new SystemException("invaild Message");
+        }
+        byte flag = buff[offset];
+        offset += MessageConstont.SizeFlag;
+        uint to = (uint)IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buff, offset));
+        offset += MessageConstont.SizeLogicAddr;
+        uint from = (uint)IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buff, offset));
+        offset += MessageConstont.SizeLogicAddr;
+        
+        var msgType = (int)flag & MessageConstont.MaskMessageType;
+
+        if(to != localLogicAddr) {
+            //当前节点非目的地
+            byte[] payload = new byte[length];
+            Buffer.BlockCopy(buff,backOffset,payload,0,length);
+            return new RelayMessage(new LogicAddr(to),new LogicAddr(from),payload);
+        } else if(msgType == MessageConstont.Msg) {
+            if(buff.Length - offset < MessageConstont.SizeCmd) {
+                throw new SystemException("invaild Message");
+            }
+            var cmd = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buff, offset));
+            offset += MessageConstont.SizeCmd;
+            var msg = ProtoMessage.Unmarshal("ss",cmd,buff,offset,endOffset-offset);
+            return new SSMessage(new LogicAddr(to),new LogicAddr(from),(ushort)cmd,msg);
+        } else if (msgType == MessageConstont.RpcReq) {
+            MemoryStream memoryStream = new MemoryStream(buff, offset, endOffset - offset);
+            Rpc.Proto.rpcRequest req = new Rpc.Proto.rpcRequest();
+            req.MergeFrom(memoryStream); 
+            return new RpcRequestMessage(new LogicAddr(to),new LogicAddr(from),req);
+        } else if (msgType == MessageConstont.RpcResp) {
+            MemoryStream memoryStream = new MemoryStream(buff, offset, endOffset - offset);
+            Rpc.Proto.rpcResponse resp = new Rpc.Proto.rpcResponse();
+            resp.MergeFrom(memoryStream);
+            return new RpcResponseMessage(new LogicAddr(to),new LogicAddr(from),resp);
+        } else {
+            throw new SystemException("invaild Message");
+        }
+    }
+}
+
+public class MessageReceiver: PacketReceiverI
+{
+    private byte[] buff;
+    private int    w = 0;
+
+    private int    r = 0;
+
+    private MessageCodecI codec;
+
+    private int maxPacketSize = 0; 
+
+    public MessageReceiver(int maxPacketSize,MessageCodecI codec)
+    {
+        if(maxPacketSize <= 0) {
+            throw new Exception("invaild MaxPacketSize");
+        }
+        this.maxPacketSize = maxPacketSize;
+        buff = new byte[this.maxPacketSize];
+        this.codec = codec;
+    }
+
+    public async Task<Object?> Recv(StreamReaderI reader)
+    {
+        for(;;)
+        {
+            var unpackSize = w-r;
+            if(unpackSize >= 4)
+            {
+                int payload =  IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buff, r));
+                int totalSize = payload+MessageConstont.SizeLen;
+                if(payload <= 0)
+                {
+                    throw new SystemException("invaild payload");
+                } else if(totalSize > maxPacketSize){
+                    throw new SystemException("packet too large");
+                } else if(totalSize <= unpackSize) {
+                    r += MessageConstont.SizeLen;
+                    Object? msg = codec.Decode(buff,r,payload);
+                    r += payload;
+                    if(r == w){
+                        r = w = 0;
+                    } 
+                    return msg;
+                } else {
+                    Buffer.BlockCopy(buff,r,buff,0,w-r);
+                    w = w - r;
+                    r = 0;
+                }
+            }
+
+            var n = await reader.Recv(buff, w, buff.Length-w);
+            if(n == 0)
+            {
+                return null;
+            } else
+            {
+                w+=n;
+            } 
+        }
     }
 }
 
--- Rpc.cs
+++ Rpc.cs
@@ -5,81 +5,359 @@ using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using System.Text;
 using System.Threading;
+using Google.Protobuf;
 namespace SanguoDotNet;
 
-public class RpcRequestMessage 
+public interface RpcCodecI 
 {
-    private UInt64 _seqno;
-    public UInt64 Seqno{get=>_seqno;}
-
-    private string _method;
-    public string Method{get=>_method;}
-
-    private byte[] _arg;
-    public byte[] Arg{get=>_arg;}
-
-    private bool _oneway = false;
-    public bool   Oneway{get=>_oneway;}   
-
-    public RpcRequestMessage(UInt64 seqno,string method,byte[] arg)
-    {
-        _seqno = seqno;
-        _method = method;
-        _arg = arg;
-    }  
-
-    public RpcRequestMessage(UInt64 seqno,string method,byte[] arg,bool oneway)
-    {
-        _seqno = seqno;
-        _method = method;
-        _arg = arg;
-        _oneway = oneway;
-    }
-
+    byte[] Encode(IMessage m);
+    void Decode(byte[] buff,IMessage m);
 }
 
-struct RpcResponseMessage
+public class RpcPbCodec : RpcCodecI
+{
+    public byte[] Encode(IMessage m)
+    {
+        MemoryStream stream = new MemoryStream();
+        m.WriteTo(stream);
+        return stream.ToArray();   
+    }
+
+
+    public void Decode(byte[] buff,IMessage m)
+    {
+        MemoryStream stream = new MemoryStream(buff);    
+        m.MergeFrom(stream);
+    }
+}
+
+public class RpcCodec
+{
+    public static RpcCodecI Codec = new RpcPbCodec();
+}
+
+
+public interface RpcChannelI
+{
+    void SendRequest(Rpc.Proto.rpcRequest request);
+    void Reply(Rpc.Proto.rpcResponse response);
+}
+
+
+/*
+
+type Client struct {
+	nextSequence uint64
+	codec        Codec
+	pendingCall  [32]sync.Map
+}
+
+func NewClient(codec Codec) *Client {
+	return &Client{
+		codec: codec,
+	}
+}
+
+func (c *Client) OnMessage(context context.Context, resp *ResponseMsg) {
+	if ctx, ok := c.pendingCall[int(resp.Seq)%len(c.pendingCall)].LoadAndDelete(resp.Seq); ok {
+		ctx.(*callContext).stopTimer()
+		ctx.(*callContext).callOnResponse(c.codec, resp.Ret, resp.Err)
+	} else {
+		logger.Infof("onResponse with no reqContext:%d", resp.Seq)
+	}
+}
+
+func (c *Client) CallWithCallback(channel Channel, deadline time.Time, method string, arg interface{}, ret interface{}, respCb RespCB) func() bool {
+	if b, err := c.codec.Encode(arg); err != nil {
+		logger.Panicf("encode error:%v", err)
+		return nil
+	} else {
+		reqMessage := &RequestMsg{
+			Seq:    atomic.AddUint64(&c.nextSequence, 1),
+			Method: method,
+			Arg:    b,
+		}
+		if respCb == nil || ret == nil {
+			reqMessage.Oneway = true
+			if err = channel.SendRequest(reqMessage, deadline); err != nil {
+				logger.Errorf("SendRequest error:%v", err)
+			}
+			return nil
+		} else {
+
+			ctx := &callContext{
+				onResponse:   respCb,
+				respReceiver: ret,
+			}
+
+			pending := &c.pendingCall[int(reqMessage.Seq)%len(c.pendingCall)]
+
+			pending.Store(reqMessage.Seq, ctx)
+
+			ctx.deadlineTimer.Store(time.AfterFunc(deadline.Sub(time.Now()), func() {
+				if _, ok := pending.LoadAndDelete(reqMessage.Seq); ok {
+					ctx.callOnResponse(c.codec, nil, newError(ErrTimeout, "timeout"))
+				}
+			}))
+
+			if err = channel.SendRequest(reqMessage, deadline); err != nil {
+				if _, ok := pending.LoadAndDelete(reqMessage.Seq); ok {
+					ctx.stopTimer()
+					if e, ok := err.(net.Error); ok && e.Timeout() {
+						go ctx.callOnResponse(c.codec, nil, newError(ErrTimeout, "timeout"))
+					} else {
+						go ctx.callOnResponse(c.codec, nil, newError(ErrSend, err.Error()))
+					}
+				}
+				return nil
+			} else {
+				return func() bool {
+					if _, ok := pending.LoadAndDelete(reqMessage.Seq); ok {
+						ctx.stopTimer()
+						return true
+					} else {
+						return false
+					}
+				}
+			}
+		}
+	}
+}
+
+func (c *Client) Call(ctx context.Context, channel Channel, method string, arg interface{}, ret interface{}) error {
+	if b, err := c.codec.Encode(arg); err != nil {
+		logger.Panicf("encode error:%v", err)
+		return nil
+	} else {
+		reqMessage := &RequestMsg{
+			Seq:    atomic.AddUint64(&c.nextSequence, 1),
+			Method: method,
+			Arg:    b,
+		}
+		if ret != nil {
+			waitC := make(chan error, 1)
+			pending := &c.pendingCall[int(reqMessage.Seq)%len(c.pendingCall)]
+
+			pending.Store(reqMessage.Seq, &callContext{
+				respReceiver: ret,
+				onResponse: func(_ interface{}, err error) {
+					waitC <- err
+				},
+			})
+
+			if err = channel.SendRequestWithContext(ctx, reqMessage); err != nil {
+				pending.Delete(reqMessage.Seq)
+				if e, ok := err.(net.Error); ok && e.Timeout() {
+					return newError(ErrTimeout, "timeout")
+				} else {
+					return newError(ErrSend, err.Error())
+				}
+			}
+
+			select {
+			case err := <-waitC:
+				return err
+			case <-ctx.Done():
+				pending.Delete(reqMessage.Seq)
+				switch ctx.Err() {
+				case context.Canceled:
+					return newError(ErrCancel, "canceled")
+				case context.DeadlineExceeded:
+					return newError(ErrTimeout, "timeout")
+				default:
+					return newError(ErrOther, "unknow")
+				}
+			}
+		} else {
+			reqMessage.Oneway = true
+			if err = channel.SendRequestWithContext(ctx, reqMessage); nil != err {
+				if e, ok := err.(net.Error); ok && e.Timeout() {
+					return newError(ErrTimeout, "timeout")
+				} else {
+					return newError(ErrSend, err.Error())
+				}
+			} else {
+				return nil
+			}
+		}
+	}
+}
+
+type callContext struct {
+	onResponse    RespCB
+	deadlineTimer atomic.Value
+	fired         int32
+	respReceiver  interface{}
+}
+
+
+	nextSequence uint64
+	codec        Codec
+	pendingCall  [32]sync.Map
+
+
+
+*/
+
+public class RpcError
 {
 
-    public class RpcError
+    public static readonly uint ErrOk = 0;
+	public static readonly uint ErrInvaildMethod = 1;
+	public static readonly uint ErrServerPause = 2;
+	public static readonly uint ErrTimeout = 3;
+	public static readonly uint ErrSend = 4;
+	public static readonly uint ErrCancel = 5;
+	public static readonly uint ErrMethod = 6;
+	public static readonly uint ErrOther = 7;
+
+
+    private uint _code = 0;
+    public uint Code{get=>_code;}
+
+    private string? _desc = null;
+    public string? Desc{get=>_desc;}
+
+    public RpcError(uint code,string desc)
     {
-        private int _code = 0;
-        public int Code{get=>_code;}
+        _code = code;   
+        _desc = desc;
+    }
+}
 
-        private string? _desc = null;
-        public string? Desc{get=>_desc;}
+public class RpcClient
+{
 
-        public RpcError(int code,string desc)
+    public class Response<Ret>
+    {
+        private RpcError? _err;
+        public RpcError? Err {get=>_err;}
+
+        private Ret? _result;
+        public Ret? Result {get=>_result;}
+    
+        public Response(Ret ret)
         {
-            _code = code;
-            _desc = desc;
+            _result = ret;
         }
 
+        public Response(RpcError err)
+        {
+            _err = err;
+        }
     }
 
-    private UInt64 _seqno;
-    public UInt64 Seqno{get=>_seqno;}
-
-    private byte[]? _ret = null;
-    public byte[]? Ret{get=>_ret;}  
-
-    private RpcError? _error = null;
-    public RpcError? Error{get=>_error;}
-
-    public RpcResponseMessage(UInt64 seqno,byte[] ret)
+    private class callContext
     {
-        _seqno = seqno;
-        _ret = ret;
-    }
+        //public int fired = 0;
 
-    public RpcResponseMessage(UInt64 seqno,RpcError err)
+        public Mutex mtx = new Mutex();
+
+        private Rpc.Proto.rpcResponse? response = null;
+
+        private SemaphoreSlim semaphore = new SemaphoreSlim(1);
+
+        public async Task<Rpc.Proto.rpcResponse?>  Wait(CancellationToken cancellationToken)
+        {
+            Rpc.Proto.rpcResponse? resp;
+            await semaphore.WaitAsync(cancellationToken);
+            mtx.WaitOne();
+            resp = response;
+            mtx.ReleaseMutex();
+            return resp;
+        }   
+
+        public void Wakeup(Rpc.Proto.rpcResponse response)
+        {
+            //if(Interlocked.CompareExchange(ref fired,1,0) == 0)
+            //{
+            mtx.WaitOne();
+            this.response = response;
+            mtx.ReleaseMutex();
+            semaphore.Release();
+            //}
+        } 
+    }
+    
+    private ulong nextSeqno = 0;
+
+    private Mutex mtx = new Mutex();
+
+    private Dictionary<ulong,callContext> pendingCall = new Dictionary<ulong,callContext>();
+
+    public void Call<Arg>(RpcChannelI channel,string method,Arg arg,CancellationToken cancellationToken) where Arg : IMessage<Arg>
     {
-        _seqno = seqno;
-        _error = err;
+        Rpc.Proto.rpcRequest request = new Rpc.Proto.rpcRequest();
+        request.Seq = Interlocked.Add(ref nextSeqno,1);
+        request.Method = method;
+        request.Arg = ByteString.CopyFrom(RpcCodec.Codec.Encode(arg));
+        request.Oneway = true;
+        channel.SendRequest(request);
     }
 
+    public async Task<Response<Ret>> Call<Arg,Ret>(RpcChannelI channel,string method,Arg arg,CancellationToken cancellationToken) where Arg : IMessage<Arg> where Ret : IMessage<Ret>,new()
+    {
+        Rpc.Proto.rpcRequest request = new Rpc.Proto.rpcRequest();
+        request.Seq = Interlocked.Add(ref nextSeqno,1);
+        request.Method = method;
+        request.Arg = ByteString.CopyFrom(RpcCodec.Codec.Encode(arg));
+        request.Oneway = false;
 
+        callContext context = new callContext();
+        mtx.WaitOne();
+        pendingCall[request.Seq] = context;
+        mtx.ReleaseMutex();
+
+        Exception? e = null;
+
+        Rpc.Proto.rpcResponse? resp = null;
+        try{
+            channel.SendRequest(request);
+            resp = await context.Wait(cancellationToken);
+        }
+        catch(Exception ee)
+        {
+            if(!cancellationToken.IsCancellationRequested){
+                e = ee;
+            }
+        }
+
+        if(resp is null) {
+            mtx.WaitOne();
+            pendingCall.Remove(request.Seq);
+            mtx.ReleaseMutex();
+            if(!(e is null)){
+                throw e;
+            } else {
+                //无法区分cancel原因，统一按超时处理
+                return new Response<Ret>(new RpcError(RpcError.ErrTimeout,"timeout"));
+            }
+        } else {
+            if(resp.ErrCode == 0) {
+                Ret ret = new Ret();
+                RpcCodec.Codec.Decode(resp.Ret.ToByteArray(),ret);
+                return new Response<Ret>(ret);
+            } else {
+                return new Response<Ret>(new RpcError(resp.ErrCode,resp.ErrDesc));
+            }
+        }
+    }
+
+    public void OnMessage(Rpc.Proto.rpcResponse respMsg)
+    {
+        mtx.WaitOne();
+        callContext? ctx = pendingCall[respMsg.Seq];
+        if(!(ctx is null)){
+            pendingCall.Remove(respMsg.Seq);
+            mtx.ReleaseMutex();
+            ctx.Wakeup(respMsg);
+        } else {
+            mtx.ReleaseMutex();
+        }
+    }
 }
 
+//public class RpcServer
+//{
 
-
+//}
